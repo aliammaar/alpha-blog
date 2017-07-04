bc55fc00bf7bab5aaf2a58ab32885f4822a19aea
alie <alie.ammaar@gmail.com>
2017-07-04 17:49:45 +0500

    Editing article is working now.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,30 @@ class ArticlesController < ApplicationController
 				render 'new'
 			end
 		end
+		
+		def show
+			@article = Article.find(params[:id])
+		end
+
+		def update
+			@article = Article.find(params[:id])
+			if @article.update(article_params)
+				flash[:notice] = "Article updated successfully"
+				redirect_to article_path(@article)
+			else
+				render 'edit'
+			end
+			
+		end
+		def edit
+			@article = Article.find(params[:id])
+			
+		end
 
 		private
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
 
-		def show
-			@article = Article.find(params[:id])
-			
-		end
 end
+
